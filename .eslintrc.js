7476cb3c842b2a235c6a41d4bc9efd6d6a34ce22
焦质晔 <jiaozhiye@163.com>
2021-02-07 11:07:19 +0800

    更新了架构代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,6 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
