0c32c10010e6412792feb1d4c57f4a92944e4dae
焦质晔 <jiaozhiye@163.com>
2021-05-17 10:03:03 +0800

    更新了架构代码

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -91,7 +91,7 @@ const removeLockingRequest = (config: IRequestConfig): void => {
 const instance: IAxiosInstance = axios.create({
   baseURL: config.baseUrl,
   timeout: 1000 * 20,
-  withCredentials: true, // 跨域请求是否携带 cookie
+  withCredentials: false, // 跨域请求是否携带 cookie
   paramsSerializer: (params): string => {
     // 序列化 GET 请求参数 -> a: [1, 2] => a[0]=1&a[1]=2
     return qs.stringify(params, { arrayFormat: 'indices' });
